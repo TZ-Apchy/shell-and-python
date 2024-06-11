5e5595d29bc5b151b4782575ee4f0f1b3b28e41c
yamgfok <2285561193@qq.com>
2024-06-11 23:47:58 +0800

    modify Test_Project

---
+++ Test_Project/import_module_multiple.py
@@ -0,0 +1,18 @@
+"""从python_programming模块中导入多个类，如这里导入Car和ElectricCar类"""
+# from python_programming import Car,ElectricCar
+#
+# my_new_car = Car("audi", "a4", 2019)
+# print(my_new_car.get_descriptive_name())
+# my_new_car.read_odometer()
+# my_new_car.odometer_reading = 30  # 通过实例访问，直接修改属性的值
+# my_new_car.read_odometer()
+# my_new_car.update_odometer(40)  # 通过update_odometer方法来修改属性的值
+# my_new_car.read_odometer()
+# my_new_car.increment_odometer(100)  # 通过increment_odometer方法对属性的值进行递增，这里在原来基础上增加了100
+# my_new_car.read_odometer()
+# my_new_car.increment_odometer(200)  # 通过increment_odometer方法对属性的值再次进行递增，这里在原来基础上增加了200
+# my_new_car.read_odometer()
+# my_tesla=ElectricCar("tesla","model s",2020)
+# print(my_tesla.get_descriptive_name())
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
---
+++ Test_Project/import_module_single.py
@@ -0,0 +1,14 @@
+"""从python_programming模块中导入单个类，如这里只导入Car类"""
+# from python_programming import Car
+#
+# my_new_car = Car("audi", "a4", 2019)
+# print(my_new_car.get_descriptive_name())
+# my_new_car.read_odometer()
+# my_new_car.odometer_reading = 30  # 通过实例访问，直接修改属性的值
+# my_new_car.read_odometer()
+# my_new_car.update_odometer(40)  # 通过update_odometer方法来修改属性的值
+# my_new_car.read_odometer()
+# my_new_car.increment_odometer(100)  # 通过increment_odometer方法对属性的值进行递增，这里在原来基础上增加了100
+# my_new_car.read_odometer()
+# my_new_car.increment_odometer(200)  # 通过increment_odometer方法对属性的值再次进行递增，这里在原来基础上增加了200
+# my_new_car.read_odometer()
