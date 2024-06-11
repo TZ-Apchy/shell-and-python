3be5c6b12a92a1d878b021900d12de1d4194e0fd
yamgfok <2285561193@qq.com>
2024-06-11 23:53:54 +0800

    modify Test_Project

---
+++ Test_Project/import_module_all.py
@@ -0,0 +1,10 @@
+"""导入整个模块，如这里导入python_programming整个模块"""
+# import python_programming
+#
+# my_new_car = python_programming.Car("audi", "a4", 2019)
+# print(my_new_car.get_descriptive_name())
+#
+# my_tesla=python_programming.ElectricCar("tesla","model s",2020)
+# print(my_tesla.get_descriptive_name())
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
