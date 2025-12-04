449aa838b075d18237edc5bae06e51b8969ef204
yamgfok <2285561193@qq.com>
2025-12-04 23:05:18 +0800

    study python 20251204

---
+++ Test_Project/day day up/day02.py
@@ -0,0 +1,11 @@
+# print(chr(98)) # 输出ASCII值
+# print(ord("唐")) # 输出"唐" 对应的unicode值
+# print("北京",end='')
+# print("欢迎你") # 将北京欢迎你输出到一行
+# print(192,168,0,1,sep=".")
+# print("北京欢迎你"+"2022")
+
+# name=input("我的姓名是：")
+# print("My name is "+name)
+# print("My name is",name)
+
---
+++ Test_Project/day day up/day03.py
@@ -0,0 +1,11 @@
+# name=input("请输入名字: ")
+# print(name)
+# l=[1,2,3]
+# l.append(4)
+# print(l)
+# print(len(l))
+# l.clear()
+# print(l)
+# print("hello","world",sep=" ",end="")
+# print("****",sep=" ")
+# print(len.__name__) # 获取函数名
